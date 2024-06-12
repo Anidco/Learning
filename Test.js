1980b11b703e511a08912493cd2adcd71ab293b2
AnidCo <107554317+Anidco@users.noreply.github.com>
2024-06-12 23:09:24 +0200

    Update and rename Test.pdf to Test.js

---
+++ Test.js
@@ -0,0 +1,8 @@
+function storeNames() {
+    return arguments;
+}
+
+// When we run the following line in the console:
+storeNames("Mulder", "Scully", "Alex Krycek");
+
+// The output will be { '0': 'Mulder', '1': 'Scully', '2': 'Alex Krycek' }
