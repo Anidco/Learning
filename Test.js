4e51241eb5f2ada3f853912a1b90a1d30f623214
AnidCo <107554317+Anidco@users.noreply.github.com>
2024-06-16 22:48:31 +0200

    Update Test.js

--- Test.js
+++ Test.js
@@ -3,6 +3,6 @@ function storeNames() {
 }
 
 // When we run the following line in the console:
-storeNames("Mina", "Bab", "Anid Co");
+storeNames("Mina", "Bab", "AnidCo");
 
 // The output will be { '0': 'Bab', '1': 'AnidCo', '2': 'Mina' }
