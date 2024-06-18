37b3e2ee9532c7bad534783e0ec7c62f8381cdc2
AnidCo <107554317+Anidco@users.noreply.github.com>
2024-06-18 21:34:11 +0200

    Update and rename Test.js to calculateHash

--- Test.js
+++
@@ -1,8 +0,0 @@
-function storeNames() {
-    return arguments;
-}
-
-// When we run the following line in the console:
-storeNames("Mina", "Bab", "AnidCo");
-
-// The output will be { '0': 'Bab', '1': 'AnidCo', '2': 'Mina' }
